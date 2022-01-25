c4364a3bbf0acb8a6ceee3aaa7c31c692bfbace5
nym0101 <nym0101@echomarketing.co.kr>
2022-01-25 22:40:28 +0900

    airflow query

--- 02-airflow/query_encryption_dynamic_tasks.py
+++ 02-airflow/query_encryption_dynamic_tasks.py
@@ -18,7 +18,7 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id="crea-pipeline-test",
+    dag_id="pipeline-test",
     default_args=default_args,
     schedule_interval='01 22  * * *',
     tags=["image"],
@@ -35,50 +35,69 @@ def aes_key():
     session = boto3.session.Session()
     client = session.client(
         service_name="secretsmanager",
-        region_name="ap-northeast-2"
+        region_name="ap-southeast-2"
     )
 
     secret_key = client.get_secret_value(
         SecretId="dev/aeskey"
     )
-    secret_dict = ast.literal_eval(secret_key.get('SecretString'))
+    secret_dict = ast.literal_eval(secret_key.get('secret'))
 
-    aes_key = secret_dict.get("AES256Key")
+    aes_key = secret_dict.get("AESKey")
     aes_key_utf = aes_key.encode('utf-8')
 
 
     iv = aes_key_utf
     pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
 
-    
-    
     request = '''
         select
-            access_tkn,
-            app_secret
+            refresh_tkn,
+            clnt_id,
+            end_point
         from
-            media_api.tb_md_api_info
+            media.api
         where
-            md_nm = 'facebook'
-            and clctr_ty = 'image'
-            and use_at = true
+            md = 'kakao'
     '''
-    mysql_hook = MySqlHook(mysql_conn_id = 'api_info')
+    mysql_hook = MySqlHook(mysql_conn_id = 'api')
     df = mysql_hook.get_pandas_df(sql=request)
-    aes = AES.new(aes_key_utf, AES.MODE_CBC, iv)
-    enc_access_token = aes.encrypt(pad(df['access_tkn'].loc[0]).encode('utf-8'))
-    tkn_byte64 = base64.b64encode(enc_access_token)
-    final_tkn = tkn_byte64.decode('utf-8')
-    print("access_tocken : {}".format(final_tkn))
-    Variable.set("fb_access_token", final_tkn)
 
-    print("formal_app_secret : {}".format(df['app_secret'].loc[0]))
-    aes = AES.new(aes_key_utf, AES.MODE_CBC, iv)
-    enc_app_secret = aes.encrypt(pad(df['app_secret'].loc[0]).encode('utf-8'))
-    app_byte64 = base64.b64encode(enc_app_secret)
-    final_app = app_byte64.decode('utf-8')
-    print("app_secret : {}".format(final_app))
-    Variable.set("fb_app_secret", final_app)
+    # refresh token 암호화 및 airflow 변수 set
+    aes = AES.new(aes_key_utf, AES.MODE, iv)
+    refresh_tkn = df['refresh_tkn'].loc[0]
+    enc_refresh_token = aes.encrypt(pad(refresh_tkn).encode('utf-8'))
+    tkn_byte64 = base64.b64encode(enc_refresh_token)
+    final_tkn = tkn_byte64.decode('utf-8')
+    print("aes_refresh_tocken : {}".format(final_tkn))
+    Variable.set("kakao_refresh_token", final_tkn)
+
+    # refresh token으로 access token 발급
+    url = "https://kauth.kakao.com/oauth/token"
+
+    body = {
+        "grant_type" : "refresh_token",
+        "client_id" : clnt_id,
+        "refresh_token" : refresh_tkn,
+    }
+
+    res = requests.post(url=url, data=body)
+
+    # response에 refresh token이 있는 경우 refresh token 업데이트
+    if("refresh_token" in res):
+        update_sql='''
+        update
+            media.api
+        set
+            refresh_tkn = {}
+        where
+            md = 'kakao'           
+        '''.format(res["refresh_token"])
+
+        mysql_hook.run(update_sql)
+        print("update access token success")
+
+    access_tkn = res["access_token"]
 
 def fetch_records():
     request = '''
@@ -88,17 +107,16 @@ def fetch_records():
             (select
                 use_acnt.acnt_id,
             from
-                media.pi_use_ty use_ty
+                media.api_use_ty use_ty
             left join
                 media.api_use_acnt use_acnt
             on
                 (use_ty.acnt_id=use_acnt.acnt_id)
             where
                 use_ty.use_at = 1
-                and use_ty.api_ty = 'image'
-                and use_acnt.use_at = 1 ) acnt
+                ) acnt
         '''
-    mysql_hook = MySqlHook(mysql_conn_id = 'api_info')
+    mysql_hook = MySqlHook(mysql_conn_id = 'api')
     df = mysql_hook.get_pandas_df(sql=request)]
     js = df.to_json(orient='records')
     Variable.set("fb_images", js)
@@ -132,8 +150,6 @@ for val in fb_accounts:
                 {
                     "name": "facebook-image-api",
                     "environment": [
-                        {"name":"accessToken", "value":Variable.get('fb_access_token')},
-                        {"name":"appSecret","value":Variable.get('fb_app_secret')},
                         {"name":"accountId","value":val.get('acnt_id')},
                         {"name":"SPRING_PROFILES_ACTIVE","value":"dev"},
                         {"name":"agoDate","value":"0"}
@@ -144,16 +160,16 @@ for val in fb_accounts:
         network_configuration={
             'awsvpcConfiguration': {
                 'subnets': [
-                    'subnet-0wfdfacdaaef022ffa32c'
+                    'subnet-0wfdfacdaaef022ffatjd32c'
                 ],
             'securityGroups': [
-                    'sg-0afefagee7c76fd81676'
+                    'sg-0afefagee7c7tjdrdj6fd81676'
                 ],
                 'assignPublicIp': 'ENABLED'
             }
         },
         awslogs_group="/ecs/facebook-image-api",
-        awslogs_region="ap-northeast-2",
+        awslogs_region="ap-southeast-2",
         awslogs_stream_prefix="ecs/facebook-image-api",  # replace with your container name
     )
     image_collector_facebook_tasks.append(image_collector_facebook)
